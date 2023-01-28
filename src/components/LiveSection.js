4677039b322a46a31c03ca141a34800437744d78
febriananur <febriananurokhman@gmail.com>
2023-01-28 19:33:05 +0700

    remove unused

--- src/components/LiveSection.js
+++ src/components/LiveSection.js
@@ -1,13 +1,7 @@
 import Card from "react-bootstrap/Card";
 function LiveSection() {
   return (
-    <div class="pt-5">
-      {/* <div class="card cardbg cardshad">
-        <div class="card-body">
-          <h5 class="card-title">LIVE CONVERSATION</h5>
-          <p>Most populare conversation</p>
-        </div>
-      </div> */}
+    <div class="pt-5 pb-5">
       <Card style={{ padding: "1.5rem", background: "#212121" }} class="rounded">
         <Card.Body>
           <Card.Title>LIVE CONVERSATION</Card.Title>
