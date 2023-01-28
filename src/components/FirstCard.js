47797d70e790dd8e0d82eceb76ccf19be548d439
febriananur <febriananurokhman@gmail.com>
2023-01-28 16:12:53 +0700

    layout

--- src/components/FirstCard.js
+++ src/components/FirstCard.js
@@ -1,23 +1,14 @@
+import Card from "react-bootstrap/Card";
+
 function FirstCard() {
   return (
-    <div class="row firstsec">
-      <div class="col-sm-4">
-        <div class="card cardbg cardshad">
-          <div class="card-body">
-            {/* card contents */}
-            <div class="row d-flex">
-              <div class="col-sm-6">
-                <h5 class="card-title">Imprestion</h5>
-              </div>
-              <div class="col-sm-6 ">{/* icon */}</div>
-            </div>
-            <div className="content">
-              <h1 class="align-content-center">80/20</h1>
-            </div>
-            <p class="card-text">Impression rate</p>
-          </div>
-        </div>
-      </div>
+    <div class="row firstsec ">
+      <Card style={{ width: "18rem", padding: "1.5rem", background: "#212121" }} class="rounded">
+        <Card.Body>
+          <Card.Title>Imprestion</Card.Title>
+          <h1>80/20</h1>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
--- src/components/LiveSection.js
+++ src/components/LiveSection.js
@@ -1,12 +1,23 @@
+import Card from "react-bootstrap/Card";
 function LiveSection() {
   return (
     <div class="pt-5">
-      <div class="card cardbg cardshad">
+      {/* <div class="card cardbg cardshad">
         <div class="card-body">
           <h5 class="card-title">LIVE CONVERSATION</h5>
           <p>Most populare conversation</p>
         </div>
-      </div>
+      </div> */}
+      <Card style={{ padding: "1.5rem", background: "#212121" }} class="rounded">
+        <Card.Body>
+          <Card.Title>LIVE CONVERSATION</Card.Title>
+          <Card.Subtitle>Most populare conversation</Card.Subtitle>
+          <div className="row content pt-3">
+            <div class="col-sm-6">t</div>
+            <div class="col-sm-6">t</div>
+          </div>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
