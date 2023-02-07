fc26cbe78ea9a5eba79bbc24e140feda9636a86e
febriananur <febriananurokhman@gmail.com>
2023-02-07 10:20:28 +0700

    posisi title

--- src/components/TrendSection.js
+++ src/components/TrendSection.js
@@ -4,10 +4,12 @@ import LineChart from "./parts/LineChart";
 function TrendSection() {
   return (
     <div class="pt-5">
-      <h4>TRENDING</h4>
-      <p>Word Trending On Spesific Time</p>
       <Card style={{ padding: "1.5rem", background: "#212121" }} class="rounded h-50 ">
-        <Card.Body><LineChart/></Card.Body>
+        <h4>TRENDING</h4>
+        <p>Word Trending On Spesific Time</p>
+        <Card.Body>
+          <LineChart />
+        </Card.Body>
       </Card>
     </div>
   );
